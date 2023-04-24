2a08112d145db0d05910cccd49ea1537f3d7d124
hayk-simonyan <hayksimonyan7@gmail.com>
2023-04-24 18:53:45 +0400

    center the create btn

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,9 +4,11 @@ import { useAuthentication } from '../hooks/useAuthentication';
 import Table from '../components/table/table';
 import Button from '../components/button';
 import LinkButton from '../components/linkButton';
+import { createUseStyles } from 'react-jss';
 
 const Home = () => {
   const { isAuthenticated, setIsAuthenticated } = useAuthentication();
+  const classes = useStyles();
 
   const handleLogout = () => {
     localStorage.removeItem('token');
@@ -15,11 +17,21 @@ const Home = () => {
 
   return (
     <>
-      {isAuthenticated && <Button title='Log Out' onClick={handleLogout} />}
-      <LinkButton title='Create Joke' path='/jokes/new' />
+      <div className={classes.container}>
+        {isAuthenticated && <Button title='Log Out' onClick={handleLogout} />}
+        <LinkButton title='Create Joke' path='/jokes/new' />
+      </div>
       <Table />
     </>
   );
 };
+
+const useStyles = createUseStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
 
 export default Home;
